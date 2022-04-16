05fd62bae3418ce6b78af0a9be1ac5d5ab94f069
guofensihua <lxl2006@mail.ustc.edu.cn>
2022-04-16 10:20:43 +0800

    init

---
+++ imports/ui/App.jsx
@@ -0,0 +1,119 @@
+import { Meteor } from "meteor/meteor";
+import React, { useState, Fragment } from "react";
+import { useTracker } from "meteor/react-meteor-data";
+import { Task } from "./Task";
+import { TasksCollection } from "../db/TasksCollection";
+import { TaskForm } from "./TaskForm";
+import { LoginForm } from "./LoginForm";
+
+export const App = () => {
+  const [hideCompleted, setHideCompleted] = useState(false);
+  const user = useTracker(() => Meteor.user());
+  const hideCompletedFilter = { isChecked: { $ne: true } };
+  // const pendingTasksCount = useTracker(() => {
+  //   if (!user) {
+  //     return 0;
+  //   }
+  //   return TasksCollection.find(hideCompletedFilter).count();
+  // });
+
+  const userFilter = user ? { userId: user._id } : {};
+  const pendingOnlyFilter = { ...hideCompletedFilter, ...userFilter };
+
+  const pendingTasksTitle = `${
+    pendingTasksCount ? ` (${pendingTasksCount})` : ""
+  }`;
+  // const tasks = useTracker(() => {
+  //   if (!user) {
+  //     return [];
+  //   }
+
+  //   return TasksCollection.find(
+  //     hideCompleted ? pendingOnlyFilter : userFilter,
+  //     {
+  //       sort: { createdAt: -1 },
+  //     }
+  //   ).fetch();
+  // });
+
+  const toggleChecked = ({ _id, isChecked }) => {
+    // TasksCollection.update(_id, {
+    //   $set: {
+    //     isChecked: !isChecked,
+    //   },
+    // });
+    Meteor.call("tasks.setIsChecked", _id, !isChecked);
+  };
+
+  // const deleteTask = ({ _id }) => TasksCollection.remove(_id);
+  const deleteTask = ({ _id }) => Meteor.call("tasks.remove", _id);
+
+  const logout = () => Meteor.logout();
+
+  const { tasks, pendingTasksCount, isLoading } = useTracker(() => {
+    const noDataAvailable = { tasks: [], pendingTasksCount: 0 };
+    if (!Meteor.user()) {
+      return noDataAvailable;
+    }
+    const handler = Meteor.subscribe("tasks");
+
+    if (!handler.ready()) {
+      return { ...noDataAvailable, isLoading: true };
+    }
+
+    const tasks = TasksCollection.find(
+      hideCompleted ? pendingOnlyFilter : userFilter,
+      {
+        sort: { createdAt: -1 },
+      }
+    ).fetch();
+    const pendingTasksCount = TasksCollection.find(pendingOnlyFilter).count();
+
+    return { tasks, pendingTasksCount };
+  });
+
+  return (
+    <div className="app">
+      <header>
+        <div className="app-bar">
+          <div className="app-header">
+            <h1>📝️ To Do List</h1>
+            {pendingTasksTitle}
+          </div>
+        </div>
+      </header>
+
+      <div className="main">
+        {user ? (
+          <Fragment>
+            <div className="user" onClick={logout}>
+              {user.username || user.profile.name} 🚪
+            </div>
+            <TaskForm user={user} />
+
+            <div className="filter">
+              <button onClick={() => setHideCompleted(!hideCompleted)}>
+                {hideCompleted ? "Show All" : "Hide Completed"}
+              </button>
+            </div>
+
+            {isLoading && <div className="loading">loading...</div>}
+
+            <ul className="tasks">
+              {tasks.map((task) => (
+                <Task
+                  key={task._id}
+                  task={task}
+                  onCheckboxClick={toggleChecked}
+                  onDeleteClick={deleteTask}
+                />
+              ))}
+            </ul>
+          </Fragment>
+        ) : (
+          <LoginForm />
+        )}
+      </div>
+    </div>
+  );
+};
